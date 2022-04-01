3e8fd85a758090dadf1a58ae9258cdb3f12e3752
Joshua M <joshua.moon10@gmail.com>
2022-04-01 19:13:14 -0400

    First commit, started logic.js, got first plate layer pulled in!

---
+++ static/js/logic.js
@@ -0,0 +1,63 @@
+// create the tile layers for the backgrounds of the map
+var defaultMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// grayscale layer
+var grayscale = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+});
+
+// water color layer
+var waterColor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'jpg'
+});
+
+// topography layer
+var topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+// make a basemaps object
+let basemaps = {
+    GrayScale: grayscale,
+    WaterColor: waterColor,
+    Topography: topoMap,
+    Default: defaultMap
+};
+
+// make a map object
+var myMap = L.map("map", {
+    center: [36.7783, -199.4179],
+    zoom: 3,
+    layers: [defaultMap, grayscale, waterColor, topoMap]
+});
+
+// add the default map to the map
+defaultMap.addTo(myMap);
+
+// add the Layer control
+L.control
+    .layers(basemaps)
+    .addTo(myMap);
+
+//  get the data for the tectonic plates and draw on the map
+// variable to hold the tectonic plates layer
+let tectonicPlates = new L.layerGroup();
+
+// call the api to get the info for the tectonic plates
+d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
+.then(function(plateData){
+    // console log to make sure the data loads
+    console.log(plateData)
+})
